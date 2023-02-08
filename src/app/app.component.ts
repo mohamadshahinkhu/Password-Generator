18c86f156bc687507ffc4e120c98037ad97f1e5f
mohamad <mohamadshahinkhu@gmail.com>
2023-02-08 16:11:48 +0330

    at the end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pass';
+
+  gnPassword: string = ''
+  includeLetters: boolean = false
+  includeNumbers: boolean = false
+  includesymbol: boolean = false
+  length!: number
+  onButtonClick() {
+    const number = '0123456789'
+    const letter = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '~!@#$%^&*()_+'
+    let validChr = ''
+    if (this.includeNumbers) {
+      validChr += number
+    }
+    if (this.includeLetters) {
+      validChr += letter
+    }
+    if (this.includesymbol) {
+      validChr += symbols
+    }
+    let generatePassword = ''
+    for (let x = 0; x < this.length; x++) {
+      let x = Math.floor(Math.random() * validChr.length)
+      generatePassword += validChr[x]
+    }
+    this.gnPassword = generatePassword
+  }
+  onchaneLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+  onchanenumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+  onchanesymbol() {
+    this.includesymbol = !this.includesymbol
+  }
+  onChangeValue(value: any) {
+    const parsedValue = parseInt(value)
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue
+    }
+  }
 }
